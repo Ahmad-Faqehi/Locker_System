0d11b01b4b66b9291c0c963df44fabb42d16d791
Ahmed Faqehi <58914905+Ahmad-Faqehi@users.noreply.github.com>
2024-10-29 23:58:34 +0300

    Update conf.php
    change the connection

--- app/inc/conf.php
+++ app/inc/conf.php
@@ -1,16 +1,16 @@
 <?php session_start();
 
 // *** THIS CONECTION WHEN USING DOCKER COMPOSE *** //
-$db_host = $_ENV['dbhost'];
-$db_user = $_ENV['dbuser'];
-$db_pass = $_ENV['dbpass'];
-$db_database = $_ENV['dbname'];
+#$db_host = $_ENV['dbhost'];
+#$db_user = $_ENV['dbuser'];
+#$db_pass = $_ENV['dbpass'];
+#$db_database = $_ENV['dbname'];
 
 // *** THIS CONECTION WHEN USING LOCAL SERVER *** //
-// $db_host = "localhost";
-// $db_user = "root";
-// $db_pass = "";
-// $db_database = "lockers";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "123321";
+$db_database = "lockers";
 
 
 $conn = new PDO('mysql:host='.$db_host.';dbname='.$db_database .';charset=utf8mb4', ''.$db_user .'', ''.$db_pass .'', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
